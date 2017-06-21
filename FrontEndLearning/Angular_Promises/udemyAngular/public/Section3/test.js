12def353742ac87a657b8b0687d568063e55237d
sandhufateh <amarfatehs@gmail.com>
2017-06-21 15:18:59 -0700

    ..

---
+++ FrontEndLearning/Angular_Promises/udemyAngular/public/Section3/test.js
@@ -0,0 +1,10 @@
+/**
+ * Created by milopreet on 6/21/17.
+ */
+
+var myName = {
+
+    name:"Amar",
+    age: 31
+
+}
---
+++ FrontEndLearning/Angular_Promises/udemyAngular/public/testWebstorm.js
@@ -0,0 +1,13 @@
+/**
+ * Created by milopreet on 6/21/17.
+ * @author Amar sandhu
+ * @
+ */
+var a = 10
+console.log("Amar sandhu testing webstorm");
+
+function test(a) {
+    console.log(a);
+}
+
+test(a);
